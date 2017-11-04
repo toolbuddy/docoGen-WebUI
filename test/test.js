4799114a1a563223f954ef35d2816ec83cc6da96
yungshenglu <yungshenglu1994@gmail.com>
2017-11-04 23:51:09 +0800

    Initial beta version

--- test/test.js
+++ test/test.js
@@ -1,6 +1,11 @@
+const docogen = require('docogen');
 const docoGen_UI = require('../main');
 
 docoGen_UI.generate_html_ex(__dirname + '/script', __dirname + '/dest', function(err, msg) {
+    console.log(msg);
+});
+
+docogen.generate_latexpdf(__dirname, __dirname + "/dest", { output: "docogen-latex" }, (err, msg) => {
     console.log(msg);
 });
 
